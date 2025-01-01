9b29c7b2769a3411c25d1257c8aad6f8308a9e27
UtopiaMinT <9797160+UtopiaMinT@users.noreply.github.com>
2025-01-01 23:49:09 +0000

    fix: show if validator isn't running; use minimal rpc methods

--- src/handlers/status.rs
+++ src/handlers/status.rs
@@ -28,6 +28,18 @@ impl Status {
             uptime_ms: 0,
         }
     }
+
+    pub fn not_running() -> Self {
+        Self {
+            hostname: hostname::get().expect("gethostname").into_string().unwrap(),
+            slot: 0,
+            identity: "(not running)".to_string(),
+            version: "".to_string(),
+            identity_balance: 0,
+            genesis_hash: "".to_string(),
+            uptime_ms: 0,
+        }
+    }
 }
 
 #[rocket::get("/status")]
@@ -37,18 +49,24 @@ pub async fn get(_auth: Certificate<'_>, config: &State<ValidatorConfig>) -> Api
         config.rpc_client.get_slot(),
         config.rpc_client.get_identity(),
         config.rpc_client.get_version(),
-        config.rpc_client.get_account(&config.primary_id),
+        config.rpc_client.get_balance(&config.primary_id),
         config.rpc_client.get_genesis_hash(),
         config.admin_client.start_time(),
     );
     let uptime_ms = start_time.unwrap().elapsed().unwrap().as_millis() as u64;
+    if slot.is_err() || identity.is_err() || version.is_err() || acct.is_err() || genesis_hash.is_err() {
+        return ApiResponder::success(
+            Some(json!(Status::unreachable())),
+            "status".to_string(),
+        );
+    }
     ApiResponder::success(
         Some(json!(Status {
             hostname: hostname::get().expect("gethostname").into_string().unwrap(),
             slot: slot.unwrap(),
             identity: identity.unwrap().to_string(),
             version: version.unwrap().to_string(),
-            identity_balance: acct.unwrap().lamports,
+            identity_balance: acct.unwrap(),
             genesis_hash: genesis_hash.unwrap().to_string(),
             uptime_ms,
         })),
